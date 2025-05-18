6437a26dfd0abba90eed11d7006f4fbfdb9b22f4
Vansh Garg <147059527+VANSHG2005@users.noreply.github.com>
2025-05-18 23:05:53 +0530

    Api Key Removed

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const lightBox = document.querySelector(".lightbox");
 const closeBtn = lightBox.querySelector(".uil-times");
 const downloadImgBtn = lightBox.querySelector(".uil-import");
 
-const apiKey = "iZzvoLcmZgvo6dZkw4tsRk30rqcRwcsOr3lF0EKap5xtDmjOrBcXmiU9";
+const apiKey = apiKey;
 const perPage = 15;
 let currentPage = 1;
 let searchTerm = null;
